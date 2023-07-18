97b2fd0beb2d5c27d03377eb43aad727cb64c2a5
Lakello <59009343+Lakello@users.noreply.github.com>
2023-07-18 17:19:41 +0700

    r

--- Assets/Source/Scripts/Enemy/EnemySpawner.cs
+++ Assets/Source/Scripts/Enemy/EnemySpawner.cs
@@ -1,21 +1,11 @@
 using UnityEngine;
 
-public class EnemySpawner : ObjectsPool<Enemy>
+public class EnemySpawner : MonoBehaviour 
 {
     [SerializeField] private SpawnPoint[] _spawnPoints;
 
     private void Awake()
     {
         _spawnPoints = GetComponentsInChildren<SpawnPoint>();
-    }
-
-    protected override void DisableObject(Enemy poolingObject)
-    {
-        throw new System.NotImplementedException();
-    }
-
-    protected override void EnableObject(Enemy poolingObject)
-    {
-        throw new System.NotImplementedException();
     }
 }
